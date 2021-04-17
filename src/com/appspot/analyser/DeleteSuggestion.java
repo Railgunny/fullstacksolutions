42da11737c7d268fbf8e99afefad75e65ff8ae7c
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-04-17 10:18:20 +0000

    uploading social interaction

--- src/com/appspot/analyser/DeleteSuggestion.java
+++ src/com/appspot/analyser/DeleteSuggestion.java
@@ -27,4 +27,13 @@ public class DeleteSuggestion extends Suggestion {
 	}
 
 	public DeleteSuggestion(String title, String description,
-			Calendar startDate, Calendar end
+			Calendar startDate, Calendar endDate, double minDuration,
+			double maxDuration, boolean isRecurring, boolean canReschedule,
+			Map<SphereName, Double> s) {
+		super(title, description, startDate, endDate, minDuration, maxDuration,
+				isRecurring, canReschedule, s);
+	}
+
+	public DeleteSuggestion(String title, String description,
+			Calendar startDate, Calendar endDate) {
+		super(title, description, startDate, en
