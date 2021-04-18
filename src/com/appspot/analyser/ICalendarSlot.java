01b048f5769a86a8df74c9ab212a96dff1995131
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-04-18 16:23:41 +0000

    rbd: add read_only rbd map option

--- src/com/appspot/analyser/ICalendarSlot.java
+++ src/com/appspot/analyser/ICalendarSlot.java
@@ -6,4 +6,13 @@ import com.google.gdata.data.TextConstruct;
 
 public interface ICalendarSlot extends Comparable<ICalendarSlot> {
 
-	public String getTitle()
+	public String getTitle();
+	public void setTitle(TextConstruct title);
+	public String getDescription();
+	public void setDescription(TextConstruct descr);
+	public Calendar getStartDate();
+	public void setStartDate(Calendar start);
+	public Calendar getEndDate();
+	public void setEndDate(Calendar end);	
+	public double getDuration();
+}
---
+++ src/com/appspot/analyser/IEvent.java
@@ -0,0 +1,5 @@
+package com.appspot.analyser;
+
+import java.util.Map;
+
+import com.a
