04f91719df096ffd933d09d39971c306a5383913
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-04-24 14:50:24 +0000

    opravy

--- src/com/appspot/analyser/InsertSuggestion.java
+++ src/com/appspot/analyser/InsertSuggestion.java
@@ -8,4 +8,12 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
-import com.appspot.datastor
+import com.appspot.datastore.SphereName;
+import com.appspot.iclifeplanning.authentication.CalendarUtils;
+import com.google.gdata.data.DateTime;
+import com.google.gdata.data.PlainTextConstruct;
+import com.google.gdata.data.calendar.CalendarEventEntry;
+import com.google.gdata.data.extensions.When;
+import com.google.gdata.util.ServiceException;
+
+public class InsertSuggestion extends Sugg
