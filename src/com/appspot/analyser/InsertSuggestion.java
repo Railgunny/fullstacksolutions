b6bf74e2ef3179fd26151463fc7ca3afc048c37f
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-04-25 15:55:23 +0000

    release 0.8.1

--- src/com/appspot/analyser/InsertSuggestion.java
+++ src/com/appspot/analyser/InsertSuggestion.java
@@ -16,4 +16,18 @@ import com.google.gdata.data.calendar.CalendarEventEntry;
 import com.google.gdata.data.extensions.When;
 import com.google.gdata.util.ServiceException;
 
-public class InsertSuggestion extends Sugg
+public class InsertSuggestion extends Suggestion {
+
+	public InsertSuggestion(BaseCalendarSlot slot) {
+		super(slot);
+	}
+
+	public InsertSuggestion(IEvent e) {
+		super(e);
+	}
+
+	public InsertSuggestion(String title, String description,
+			Calendar startDate, Calendar endDate, double minDuration,
+			double maxDuration, boolean isRecurring, boolean canReschedule,
+			Map<SphereName, Double> s) {
+		super(title, description, startDate, endDate, 
