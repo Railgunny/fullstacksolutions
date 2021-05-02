055378b886ac94e837022146d648af0a7fffd482
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-05-02 12:58:41 +0000

    simplify.

--- src/com/appspot/analyser/Proposal.java
+++ src/com/appspot/analyser/Proposal.java
@@ -2,4 +2,15 @@ package com.appspot.analyser;
 
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.ut
+import java.util.Map;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.annotations.NotPersistent;
+import javax.jdo.annotations.PersistenceCapable;
+import javax.jdo.annotations.Persistent;
+
+import com.appspot.datastore.PMF;
+import com.appspot.datastore.SphereName;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.User;
+import com.google.gdata.data.calend
