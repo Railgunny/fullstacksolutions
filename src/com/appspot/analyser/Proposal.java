b2b78b2d58f76cfa7dd694170c4762fc510a3302
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-05-08 08:48:09 +0000

    Removed stuff

--- src/com/appspot/analyser/Proposal.java
+++ src/com/appspot/analyser/Proposal.java
@@ -13,4 +13,18 @@ import com.appspot.datastore.PMF;
 import com.appspot.datastore.SphereName;
 import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
-import com.google.gdata.data.calend
+import com.google.gdata.data.calendar.CalendarEventEntry;
+
+
+@PersistenceCapable
+public class Proposal extends BaseCalendarSlot implements IEvent  {
+
+	@NotPersistent
+	private Boolean isRecurring;
+	@NotPersistent
+	private Boolean canReschedule;
+	@Persistent(serialized="true", defaultFetchGroup = "true")
+	private Map<SphereName, Double> spheres;
+	@Persistent
+	private SphereName majorSphere;
+	@Persistent(serialized="true"
