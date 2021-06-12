d99ea2e70b3198f8f3b55be6725a3430a1652591
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-06-12 06:37:51 +0000

    add basic README

--- src/com/appspot/iclifeplanning/SettingsServlet.java
+++ src/com/appspot/iclifeplanning/SettingsServlet.java
@@ -1,3 +1,16 @@
 package com.appspot.iclifeplanning;
 
-import java.io.IOExce
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.appspot.datastore.Sph
