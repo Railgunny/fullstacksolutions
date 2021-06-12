d24d52d88201ccdd2dad08dcd50faed9d2fcaa95
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-06-12 11:03:34 +0000

    Updated IPv8 pointer

--- src/com/appspot/iclifeplanning/SettingsServlet.java
+++ src/com/appspot/iclifeplanning/SettingsServlet.java
@@ -13,4 +13,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.appspot.datastore.Sph
+import com.appspot.datastore.SphereName;
+import com.appspot.datastore.UserDesiredLifeBalance;
+import com.appspot.datastore.UserDesiredLifeBalanceStore;
+import com.appspot.datastore.UserProfile;
+import com.appspot.datastore.UserProfileStore;
+import com.appspot.iclifeplanning.authentication.AuthService;
+import com.appspot.iclifeplanning.authentication.CalendarUtils;
+import com.google.ap
