67bdb883c866fe8378c70c8b1a8cef367606ca40
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-06-29 09:45:13 +0000

    update privacy policy

--- src/com/appspot/iclifeplanning/SuggestionServlet.java
+++ src/com/appspot/iclifeplanning/SuggestionServlet.java
@@ -4,4 +4,16 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Hash
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.appspot.analyser.Analyser;
+import com.appspot.analyser.DeleteSuggestion;
+import com.appspot.analyser.IEvent;
+import com.appspot.analyser.Pair;
+import com.appspot.analys
