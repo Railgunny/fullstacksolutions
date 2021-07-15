9a6ba94353a421908676a1ed7dcf0b241b2e7d39
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-07-15 19:28:21 +0000

    New translations 03_p01_ch01_01.md (Chinese Simplified)

--- src/com/appspot/iclifeplanning/SuggestionServlet.java
+++ src/com/appspot/iclifeplanning/SuggestionServlet.java
@@ -152,4 +152,9 @@ public class SuggestionServlet extends HttpServlet {
 			
 			SimpleDateFormat date = new SimpleDateFormat("EEE, d MMM yyyy HH:mm");
 			suggestionObject.put("startDateTime", date.format(suggestion.getStartDate().getTime()));
-			suggestionObject.put("endDateTime", date.format(suggestion.getEn
+			suggestionObject.put("endDateTime", date.format(suggestion.getEndDate().getTime()));
+			if(suggestion instanceof RescheduleSuggestion) {
+				suggestionObject.put("newStartDateTime", date.format(((RescheduleSuggestion)suggestion).getNewDates().getFirst()));
+				suggestionObject.put("newEndDateTime", date.format(((RescheduleSuggestion)suggestion).getNewDates().getSecond()));
+			}
+			suggestionObject.put("type", suggestion.getType());
