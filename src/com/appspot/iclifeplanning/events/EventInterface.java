014fb1a57fc36129285a763f05ba68a874ce714d
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-08-07 22:52:11 +0000

    sadsad

--- src/com/appspot/iclifeplanning/events/EventInterface.java
+++ src/com/appspot/iclifeplanning/events/EventInterface.java
@@ -3,4 +3,15 @@ package com.appspot.iclifeplanning.events;
 import java.util.Map;
 import java.util.Set;
 
-import com.appspot.datastore
+import com.appspot.datastore.SphereName;
+import com.google.gdata.data.DateTime;
+import com.google.gdata.data.TextConstruct;
+
+public interface EventInterface {
+
+	public TextConstruct getDescription();
+	public void setDescription(TextConstruct description);
+	public DateTime getStartTime();
+	public void setStartTime(DateTime startTime);
+	public DateTime getEndTime();
+	public void s
