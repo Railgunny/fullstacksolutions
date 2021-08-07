0885bb4ca5ecc6cd75dec990f784b53377b99cdc
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-08-07 12:35:29 +0000

    updated CHANGELOG.txt

--- src/com/appspot/iclifeplanning/charts/utils/WeeklyDataProfile.java
+++ src/com/appspot/iclifeplanning/charts/utils/WeeklyDataProfile.java
@@ -1,1 +1,21 @@
-package com.appspot.iclifeplanning.charts.u
+package com.appspot.iclifeplanning.charts.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.jdo.annotations.*;
+
+import com.appspot.datastore.SphereName;
+
+@PersistenceCapable
+public class WeeklyDataProfile {
+	
+	@PrimaryKey
+	@Persistent
+	private String key;
+	@Persistent
+	private String userID;
+	@Persistent
+	private Integer weekNumber;
+	@Persistent(serialized="true", defaultFetchGroup = "true")
+	private HashMap<SphereName, Doub
