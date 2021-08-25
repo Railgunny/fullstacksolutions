1e932a14abb2aa3a94b75d0d17360787a14c654e
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-08-25 15:15:00 +0000

    Regenerated gemspec for version 0.4.3

--- src/com/appspot/iclifeplanning/events/UClasifier.java
+++ src/com/appspot/iclifeplanning/events/UClasifier.java
@@ -7,4 +7,13 @@ import java.util.Map;
 
 import com.google.code.uclassify.client.UClassifyClient;
 import com.google.code.uclassify.client.UClassifyClientFactory;
-import com.uclassify.api._1
+import com.uclassify.api._1.responseschema.Classification;
+import com.uclassify.api._1.responseschema.Class;
+
+public class UClasifier {
+	private static final String USER_NAME = "iclifeplanning";
+	private static final String READ_KEY = "JPP7UqHe3LbTTWnpuPpUUNK24SE";
+	private static final String WRITE_KEY = "QQNFLRrBi2U5JL0fxFdzGadWg40";
+	private static final String SPHERES_CLASIFIER = "Spheres";
+
+	public static Map<String, Double
