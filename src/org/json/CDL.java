da6657b4b53a50b818032a80b2526e56413a66d7
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-09-15 18:53:48 +0000

    Modified comments

---
+++ src/org/json/CDL.java
@@ -0,0 +1,9 @@
+package org.json;
+
+/*
+Copyright (c) 2002 JSON.org
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, pub
