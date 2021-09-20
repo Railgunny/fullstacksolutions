1cfcd42a54bb5cc10bfc8a0834a4f67ab3e714d9
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-09-20 20:18:32 +0000

    Re-gen.

--- src/org/json/CDL.java
+++ src/org/json/CDL.java
@@ -137,4 +137,14 @@ public class CDL {
     /**
      * Produce a JSONArray of JSONObjects from a comma delimited text string,
      * using the first row as a source of names.
-     * @pa
+     * @param string The comma delimited text.
+     * @return A JSONArray of JSONObjects.
+     * @throws JSONException
+     */
+    public static JSONArray toJSONArray(String string) throws JSONException {
+        return toJSONArray(new JSONTokener(string));
+    }
+
+    /**
+     * Produce a JSONArray of JSONObjects from a comma delimited text string,
+     * using the first row as a source 
