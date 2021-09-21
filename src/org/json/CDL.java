949e48dff469e96e9904022dc75d5335dd2ba5da
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-09-21 09:24:24 +0000

    Fix #74

--- src/org/json/CDL.java
+++ src/org/json/CDL.java
@@ -158,4 +158,13 @@ public class CDL {
 
     /**
      * Produce a JSONArray of JSONObjects from a comma delimited text string
-     *
+     * using a supplied JSONArray as the source of element names.
+     * @param names A JSONArray of strings.
+     * @param string The comma delimited text.
+     * @return A JSONArray of JSONObjects.
+     * @throws JSONException
+     */
+    public static JSONArray toJSONArray(JSONArray names, String string)
+            throws JSONException {
+        return toJSONArray(names, new JSONTokener(string));
+   
