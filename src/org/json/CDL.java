73768a688abd428614cc406d2eec14bb162a9536
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-09-21 16:28:59 +0000

    Update raidcompanion.html

--- src/org/json/CDL.java
+++ src/org/json/CDL.java
@@ -167,4 +167,14 @@ public class CDL {
     public static JSONArray toJSONArray(JSONArray names, String string)
             throws JSONException {
         return toJSONArray(names, new JSONTokener(string));
-   
+    }
+
+    /**
+     * Produce a JSONArray of JSONObjects from a comma delimited text string
+     * using a supplied JSONArray as the source of element names.
+     * @param names A JSONArray of strings.
+     * @param x A JSONTokener of the source text.
+     * @return A JSONArray of JSONObjects.
+     * @throws JSONException
+     */
+    public static JSONArray toJSONArray
