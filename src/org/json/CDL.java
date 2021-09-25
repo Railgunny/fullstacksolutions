9fce4c34b2cd5790961d21b362d53af382d5503c
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-09-25 19:37:48 +0000

    mm: process reclaim: vmpressure based process reclaim

--- src/org/json/CDL.java
+++ src/org/json/CDL.java
@@ -240,4 +240,15 @@ public class CDL {
      * JSONObject.
      * @param ja A JSONArray of JSONObjects.
      * @return A comma delimited text.
- 
+     * @throws JSONException
+     */
+    public static String toString(JSONArray ja) throws JSONException {
+        JSONObject jo = ja.optJSONObject(0);
+        if (jo != null) {
+            JSONArray names = jo.names();
+            if (names != null) {
+                return rowToString(names) + toString(names, ja);
+            }
+        }
+        return null;
+    }
