5989e42564bbcc0e44264cb5fd75995502e161f6
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-09-26 05:57:28 +0000

    Add entry to CHANGELOG

--- src/org/json/CDL.java
+++ src/org/json/CDL.java
@@ -252,3 +252,14 @@ public class CDL {
         }
         return null;
     }
+
+    /**
+     * Produce a comma delimited text from a JSONArray of JSONObjects using
+     * a provided list of names. The list of names is not included in the
+     * output.
+     * @param names A JSONArray of strings.
+     * @param ja A JSONArray of JSONObjects.
+     * @return A comma delimited text.
+     * @throws JSONException
+     */
+    public static String toString(JSONArray name
