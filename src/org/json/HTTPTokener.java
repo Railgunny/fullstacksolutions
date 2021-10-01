da6505b5d7410b57536253fb54d253054d11a721
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-10-01 16:53:52 +0000

    Revert "vfs: umount_tree() might be called on subtree that had never made it"

--- src/org/json/HTTPTokener.java
+++ src/org/json/HTTPTokener.java
@@ -52,4 +52,16 @@ public class HTTPTokener extends JSONTokener {
         StringBuffer sb = new StringBuffer();
         do {
             c = next();
-       
+        } while (Character.isWhitespace(c));
+        if (c == '"' || c == '\'') {
+            q = c;
+            for (;;) {
+                c = next();
+                if (c < ' ') {
+                    throw syntaxError("Unterminated string.");
+                }
+                if (c == q) {
+                    return sb.toString();
+                }
+                sb.append(c);
+  
