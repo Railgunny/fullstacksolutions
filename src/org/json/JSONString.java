5c6e9553cf73caf06f01fa0d36caec9b8ef516f2
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-10-13 21:40:51 +0000

    rtnetlink: Mask the rta_type when range checking

--- src/org/json/JSONString.java
+++ src/org/json/JSONString.java
@@ -5,4 +5,14 @@ package org.json;
  * <code>JSONObject.toString()</code>, <code>JSONArray.toString()</code>,
  * and <code>JSONWriter.value(</code>Object<code>)</code>. The 
  * <code>toJSONString</code> method will be used instead of the default behavior 
- * of using the Obje
+ * of using the Object's <code>toString()</code> method and quoting the result.
+ */
+public interface JSONString {
+	/**
+	 * The <code>toJSONString</code> method allows a class to produce its own JSON 
+	 * serialization. 
+	 * 
+	 * @return A strictly syntactically correct JSON text.
+	 */
+	public String toJSONString();
+}
---
+++ src/org/json/JSONStringer.java
@@ -0,0 +1,6 @@
+package org.json;
+
+/*
+Copyright (c) 2006 JSON.org
+
+Permission is hereby granted, fre
