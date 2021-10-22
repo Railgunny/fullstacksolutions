5736a35beb240ae95e41f80ff0019bcaf6a4bb28
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-10-22 16:23:28 +0000

    Add gist id

--- src/org/json/Test.java
+++ src/org/json/Test.java
@@ -7,4 +7,22 @@ import java.io.StringWriter;
 
 /**
  * Test class. This file is not formally a member of the org.json library.
- * It is just a casual test tool
+ * It is just a casual test tool.
+ */
+public class Test {
+	
+    /**
+     * Entry point.
+     * @param args
+     */
+    public static void main(String args[]) {
+        Iterator it;
+        JSONArray a;
+        JSONObject j;
+        JSONStringer jj;
+        Object o;
+        String s;
+        
+/** 
+ *  Obj is a typical class that implements JSONString. It also
+ *  provides some beanie methods that can be u
