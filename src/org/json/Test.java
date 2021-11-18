d5d694263a2ed1ffc5d3a77f6338f76fa6211d2b
Railgunny <112671205+Railgunny@users.noreply.github.com>
2021-11-18 15:20:21 +0000

    Synchronized attachments

--- src/org/json/Test.java
+++ src/org/json/Test.java
@@ -302,4 +302,4 @@ public class Test {
             System.out.println(XML.toString(j));
             System.out.println("");
 
-            j = XML.toJSONObject("<!DOCTYPE bCard 'http://www.cs.caltech.edu/~adam/schemas/bCard'><bCard><?xml default bCard        firstname = ''  
+            j = XML.toJSONObject("<!DOCTYPE bCard 'http://www.cs.caltech.edu/~adam/schemas/bCard'><bCard><?xml default bCard        firstname = ''        lastname  = '' company   = '' email = '' homepage  = ''?><bCard        firstname = 'Rohit'        lastname  = 'Khare'        company   = 'MCI'        email     = 'khare@mci.net'        homepage  = 'http://pest.w3.org/'/><bCard        firstname = 'Adam'        lastname  = 'Rifkin'        company   = 'Caltech Infospheres Project'        email     = 'ada
