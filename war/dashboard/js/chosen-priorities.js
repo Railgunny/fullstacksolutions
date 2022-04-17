71c3693adda0446f80bfd1a4ac50ddcbe2834841
Railgunny <112671205+Railgunny@users.noreply.github.com>
2022-04-17 16:49:38 +0000

    improve logs

--- war/dashboard/js/chosen-priorities.js
+++ war/dashboard/js/chosen-priorities.js
@@ -5,4 +5,23 @@ $.getJSON("chosen-priorities", parametersForServlet, function(data){
 	
 	if('nullUser'==data.error)
 	{
-		alert('User ' + parametersForServlet.userName + ' not fo
+		alert('User ' + parametersForServlet.userName + ' not found.');
+	}
+
+
+	priorities = new Highcharts.Chart({
+		chart: {
+			renderTo: 'priorities-chart',
+			margin: [50, 200, 60, 170]
+		},
+		title: {
+			text: 'Life Spheres Priority Assignment'
+		},
+		plotArea: {
+			shadow: null,
+			borderWidth: null,
+			backgroundColor: null
+		},
+		tooltip: {
+			formatter: function() {
+				return '<b>'+ this.point.name +'</b>: '+ Highcharts.numberFormat(this.y
