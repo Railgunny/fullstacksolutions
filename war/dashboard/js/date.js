1037dc5a00ca77fb2a2f1821a030de288c72a161
Railgunny <112671205+Railgunny@users.noreply.github.com>
2022-04-24 16:43:27 +0000

    Update State of art.md

--- war/dashboard/js/date.js
+++ war/dashboard/js/date.js
@@ -2,4 +2,10 @@
  * Date prototype extensions. Doesn't depend on any
  * other code. Doens't overwrite existing methods.
  *
- * Adds dayNames, abbrDayNames, monthName
+ * Adds dayNames, abbrDayNames, monthNames and abbrMonthNames static properties and isLeapYear,
+ * isWeekend, isWeekDay, getDaysInMonth, getDayName, getMonthName, getDayOfYear, getWeekOfYear,
+ * setDayOfYear, addYears, addMonths, addDays, addHours, addMinutes, addSeconds methods
+ *
+ * Copyright (c) 2006 Jörn Zaefferer and Brandon Aaron (brandon.aaron@gmail.com || http://brandonaaron.net)
+ *
+ * Additional methods and prop
