306206ce20c9948fb7ed850efc2ed42759a43e04
Railgunny <112671205+Railgunny@users.noreply.github.com>
2022-06-17 20:24:30 +0000

    Delete 4.jpg

--- war/dashboard/js/drawer.js
+++ war/dashboard/js/drawer.js
@@ -12,4 +12,17 @@
 $.extend({ drawer: {
 	init: function () {
 		this.self   = null;
-		this.closer = '<div id="drw_close"><a href="#" cla
+		this.closer = '<div id="drw_close"><a href="#" class="drw_close" title="close">Hide</a></div>';
+		this.loader = '<div id="drw_loader">Loading...</div>';
+		this.setTabs();
+		$('#drw').html('<div id="drw_content"></div>');
+	},
+	
+	
+	
+	setTabs: function () {
+		var tabs  = $('#drw_tabs li');
+		var w     = Math.floor(100 / tabs.size());
+		var w_rem = 100 % tabs.size();
+		tabs.each(function () {
+			$(this).css({ width: (w + ((w_rem-- >
