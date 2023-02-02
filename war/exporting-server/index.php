f5c7f172236554b1bac76b8ab7b2566e8f6a0762
Railgunny <112671205+Railgunny@users.noreply.github.com>
2023-02-02 15:03:56 +0000

    Update yml

--- war/exporting-server/index.php
+++ war/exporting-server/index.php
@@ -1,4 +1,19 @@
 <?php
 /**
  * This file is part of the exporting module for Highcharts JS.
- * www.highcharts.co
+ * www.highcharts.com/license
+ * 
+ *  
+ * Available POST variables:
+ *
+ * $filename string The desired filename without extension
+ * $type string The MIME type for export. 
+ * $width int The pixel width of the exported raster image. The height is calculated.
+ * $svg string The SVG source code to convert.
+ */
+
+
+// Options
+define ('BATIK_PATH', 'batik-rasterizer.jar');
+
+////////////////////////////////////
