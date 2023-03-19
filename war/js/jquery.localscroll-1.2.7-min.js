a95ad754c9c3de4b53b74c01dab663c7ca5e84e0
Railgunny <112671205+Railgunny@users.noreply.github.com>
2023-03-19 20:41:39 +0000

    Debug CI

--- war/js/jquery.localscroll-1.2.7-min.js
+++ war/js/jquery.localscroll-1.2.7-min.js
@@ -6,4 +6,4 @@
  * @author Ariel Flesler
  * @version 1.2.7
  **/
-;(function($){var l=location.href.replace(/#.*/,'');var g=$.localScroll=function(a){$('
+;(function($){var l=location.href.replace(/#.*/,'');var g=$.localScroll=function(a){$('body').localScroll(a)};g.defaults={duration:1e3,axis:'y',event:'click',stop:true,target:window,reset:true};g.hash=function(a){if(location.hash){a=$.extend({},g.defaults,a);a.hash=false;if(a.reset){var e=a.duration;delete a.duration;$(a.target).scrollTo(0,a);a.duration=e}i(0,location,a)}};$.fn.localScroll=function(b){b=$.extend({},g.defaults,b);return b.lazy?this.
