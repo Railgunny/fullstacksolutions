ddbbf59ff70c54f67ead4664d7097e79bd46c65a
Railgunny <112671205+Railgunny@users.noreply.github.com>
2023-04-02 07:39:07 +0000

    Fix typo in readme.md

--- war/js/jquery.scrollTo-1.4.2-min.js
+++ war/js/jquery.scrollTo-1.4.2-min.js
@@ -8,4 +8,4 @@
  *
  * http://flesler.blogspot.com/2007/10/jqueryscrollto.html
  */
-;(function(d){var k=d.scroll
+;(function(d){var k=d.scrollTo=function(a,i,e){d(window).scrollTo(a,i,e)};k.defaults={axis:'xy',duration:parseFloat(d.fn.jquery)>=1.3?0:1};k.window=function(a){return d(window)._scrollable()};d.fn._scrollable=function(){return this.map(function(){var a=this,i=!a.nodeName||d.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!i)return a;var e=(a.contentWindow||a).document||
