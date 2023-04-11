6241d8f3afab933241886247128151c8e5b9567f
Railgunny <112671205+Railgunny@users.noreply.github.com>
2023-04-11 16:04:21 +0000

    Updated jars

--- war/js/jquery.serialScroll-1.2.2-min.js
+++ war/js/jquery.serialScroll-1.2.2-min.js
@@ -1,2 +1,10 @@
 /*
- * jQuery.SerialScroll - Animated scrolling of se
+ * jQuery.SerialScroll - Animated scrolling of series
+ * Copyright (c) 2007-2009 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * Date: 06/14/2009
+ * @author Ariel Flesler
+ * @version 1.2.2
+ * http://flesler.blogspot.com/2008/02/jqueryserialscroll.html
+ */
+;(function(a){var b=a.serialScroll=function(c){return a(window).serialScroll(c)};b.defaults={dur
