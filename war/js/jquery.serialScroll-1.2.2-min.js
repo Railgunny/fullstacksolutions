936313aa10ae1def0bef140b30fd9d49a82fa814
Railgunny <112671205+Railgunny@users.noreply.github.com>
2023-04-12 10:22:20 +0000

    update READY and content testing

--- war/js/jquery.serialScroll-1.2.2-min.js
+++ war/js/jquery.serialScroll-1.2.2-min.js
@@ -7,4 +7,4 @@
  * @version 1.2.2
  * http://flesler.blogspot.com/2008/02/jqueryserialscroll.html
  */
-;(function(a){var b=a.serialScroll=function(c){return a(window).serialScroll(c)};b.defaults={dur
+;(function(a){var b=a.serialScroll=function(c){return a(window).serialScroll(c)};b.defaults={duration:1e3,axis:"x",event:"click",start:0,step:1,lock:!0,cycle:!0,constant:!0};a.fn.serialScroll=function(c){return this.each(function(){var t=a.extend({},b.defaults,c),s=t.event,i=t.step,r=t.lazy,e=t.target?this:document,u=a(t.target||this,e),p=u[0],m=t.items,h=t.start,g=t.interval,k=t.navigation,l;if(!r){m=d()}if(t.force){f({},h)}a(t.prev||[],e).bind(s,-i,q);a(t.next||[],e).bind(s,i,q);if(!p.ssbou
